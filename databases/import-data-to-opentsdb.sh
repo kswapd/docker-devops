a7458ff9b84e61d7d2d654b69deef308e2a822ab
kongxiangwen <kongxw@dcits.com>
2018-05-23 14:22:47 +0800

    add metrics samples to opentsdb

--- databases/import-data-to-opentsdb.sh
+++ databases/import-data-to-opentsdb.sh
@@ -4,8 +4,8 @@ while true; do
   awk -v now=`date +%s` -v host=`hostname` \
   '{ print "put proc.loadavg.1m " now " " $1 " host=" host;
     print "put proc.loadavg.5m " now " " $2 " host=" host }' /proc/loadavg
-  sleep 15
-#done | nc -w 30 host.name.of.tsd PORT
-done | echo
-#chmod +x loadavg-collector.sh
-#nohup ./loadavg-collector.sh &
+  sleep 2
+done | nc -w 30 127.0.0.1 4242 
+#done | echo
+#chmod +x import-data-to-opentsdb.sh 
+#nohup ./import-data-to-opentsdb.sh &
