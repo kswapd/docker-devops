ccb565de8081f3dfcc69a09c238dd550fe00f89c
kongxiangwen <xw.kong@hnair.com>
2018-07-05 16:24:25 +0800

    ok

--- icbc-devops/cmd3.sh
+++ icbc-devops/cmd3.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #nohup /home/kxw/dev/docker-devops/icbc-devops/cmd1.sh 2>&1 &
-/home/kxw/dev/docker-devops/icbc-devops/cmd1.sh run
+#/home/kxw/dev/docker-devops/icbc-devops/cmd1.sh run
+
+source /home/kxw/dev/docker-devops/icbc-devops/cmd1.sh run
 i=100
-while(true)
+while(false)
 do
 	echo "cmd2",$i
 	let ++i
